03aecf06805d52b50c33e602032ed8227a3ffebc
joealmond <jozsef.mandula@gmail.com>
2021-11-24 01:24:15 +0100

    till 11b

--- 11 Prime Numbers.js
+++ 11 Prime Numbers.js
@@ -1,32 +1,31 @@
-// showPrimes(20);
+showPrimes(20);
 
-// function showPrimes(limit) {
-//   let isPrime = true;
-//   for (let number = 2; number < limit; number++) {
-//     for (let factor = 2; factor < number; factor++) {
-//       if (number % factor === 0) {
-//         isPrime = false;
-//       }
-//     }
-//     if (isPrime === true) {
-//       console.log(number);
-//     }
-//     isPrime = true;
-//   }
-// }
+function showPrimes(limit) {
+  let isPrime = true;
+  for (let number = 2; number < limit; number++) {
+    for (let factor = 2; factor < number; factor++) {
+      if (number % factor === 0) {
+        isPrime = false;
+      }
+    }
+    if (isPrime === true) {
+      console.log(number);
+    }
+    isPrime = true;
+  }
+}
 
 // Mosh's
 showPrimes1(20);
 
 function showPrimes1(limit) {
   for (let number = 2; number < limit; number++) {
-    let isPrime = true;
-    for (let factor = 2; factor < number; factor++) {
-      if (number % factor === 0) {
-        isPrime = false;
-        break;
-      }
-    }
-    if (isPrime === true) console.log(number);
+    if (isPrime(number)) console.log(number);
   }
+}
+
+function isPrime(number) {
+  for (let factor = 2; factor < number; factor++)
+    if (number % factor === 0) return false;
+  return true;
 }
